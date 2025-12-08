ce0face72e956d66d37fe777dcc20c0ac2bd6317
Amiruli <amirul.ei@gmail.com>
2025-12-08 13:56:52 +0600

    Created Order page, Created Header component and implimented Link component for faster page load (No reload page loading)

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -1,7 +1,9 @@
 import { Route, Routes } from 'react-router'
 import { HomePage } from './pages/HomePage'
 import { CheckoutPage } from './pages/CheckoutPage'
+import { OrdersPage } from './pages/OrdersPage'
 import './App.css'
+
 
 function App() {
 
@@ -9,6 +11,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="checkout" element={<CheckoutPage />}></Route>
+      <Route path="orders" element={<OrdersPage />}></Route>
     </Routes>
     
   )
