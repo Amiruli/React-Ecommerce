3269609459f35f3e8de462ba9c3c60646bd70390
Amiruli <amirul.ei@gmail.com>
2025-12-12 00:14:04 +0600

    Order page completed

--- src/pages/OrdersPage.jsx
+++ src/pages/OrdersPage.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import dayjs from "dayjs";
-import { useEffect, useState } from "react";
+import { useEffect, useState, Fragment } from "react";
 import { Header } from "../components/Header";
 import "./OrderPage.css";
 import { formatMoney } from "../utils/money";
@@ -27,88 +27,70 @@ export function OrdersPage({ cart = [] }) {
 
           {orders.map((order) => {
             return (
-              <div key={ order.id } className="order-container">
+              <div key={order.id} className="order-container">
                 <div className="order-header">
                   <div className="order-header-left-section">
                     <div className="order-date">
                       <div className="order-header-label">Order Placed:</div>
-                      <div>{ dayjs(order.orderTimeMs).format('MMMM D') }</div>
+                      <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
                     </div>
                     <div className="order-total">
                       <div className="order-header-label">Total:</div>
-                      <div>{ formatMoney(order.totalCostCents) }</div>
+                      <div>{formatMoney(order.totalCostCents)}</div>
                     </div>
                   </div>
 
                   <div className="order-header-right-section">
                     <div className="order-header-label">Order ID:</div>
-                    <div>{ order.id }</div>
+                    <div>{order.id}</div>
                   </div>
                 </div>
 
                 <div className="order-details-grid">
-                  <div className="product-image-container">
-                    <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
-                  </div>
 
-                  <div className="product-details">
-                    <div className="product-name">
-                      Black and Gray Athletic Cotton Socks - 6 Pairs
-                    </div>
-                    <div className="product-delivery-date">
-                      Arriving on: August 15
-                    </div>
-                    <div className="product-quantity">Quantity: 1</div>
-                    <button className="buy-again-button button-primary">
-                      <img
-                        className="buy-again-icon"
-                        src="images/icons/buy-again.png"
-                      />
-                      <span className="buy-again-message">Add to Cart</span>
-                    </button>
-                  </div>
+                  {order.products.map((orderProduct) => {
+                    return(
+                    <Fragment key={orderProduct.id}>
+                      <div className="product-image-container">
+                        <img src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+                      </div>
 
-                  <div className="product-actions">
-                    <a href="tracking.html">
-                      <button className="track-package-button button-secondary">
-                        Track package
-                      </button>
-                    </a>
-                  </div>
+                      <div className="product-details">
+                        <div className="product-name">
+                          { orderProduct.name }
+                        </div>
+                        <div className="product-delivery-date">
+                          Arriving on: { dayjs(orderProduct.estimatedDeliveryTimeMs).format('MMMM D, YYYY') }
+                        </div>
+                        <div className="product-quantity">Quantity: { orderProduct.quantity }</div>
+                        <button className="buy-again-button button-primary">
+                          <img
+                            className="buy-again-icon"
+                            src="images/icons/buy-again.png"
+                          />
+                          <span className="buy-again-message">Add to Cart</span>
+                        </button>
+                      </div>
 
-                  <div className="product-image-container">
-                    <img src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-                  </div>
+                      <div className="product-actions">
+                        <a href="tracking.html">
+                          <button className="track-package-button button-secondary">
+                            Track package
+                          </button>
+                        </a>
+                      </div>
+                    </Fragment>
+                    )
+                  })}
 
-                  <div className="product-details">
-                    <div className="product-name">
-                      Adults Plain Cotton T-Shirt - 2 Pack
-                    </div>
-                    <div className="product-delivery-date">
-                      Arriving on: August 19
-                    </div>
-                    <div className="product-quantity">Quantity: 2</div>
-                    <button className="buy-again-button button-primary">
-                      <img
-                        className="buy-again-icon"
-                        src="images/icons/buy-again.png"
-                      />
-                      <span className="buy-again-message">Add to Cart</span>
-                    </button>
-                  </div>
+                  
 
-                  <div className="product-actions">
-                    <a href="tracking.html">
-                      <button className="track-package-button button-secondary">
-                        Track package
-                      </button>
-                    </a>
-                  </div>
+                  
                 </div>
               </div>
             );
           })}
-          
+
         </div>
       </div>
     </>
